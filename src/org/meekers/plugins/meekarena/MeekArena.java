ebbb750d9153ffd6f1f973c98f904ab4dd0fc025
Jared Meeker <jared@meekers.org>
2012-12-22 16:44:30 -0700

    Bug fixes galore.

--- src/org/meekers/plugins/meekarena/MeekArena.java
+++ src/org/meekers/plugins/meekarena/MeekArena.java
@@ -29,10 +29,8 @@ public class MeekArena extends JavaPlugin {
     Map<String, ItemStack[]> inventories = new HashMap<>();
     Map<String, ItemStack[]> armors = new HashMap<>();
     Map<String, Location> locations = new HashMap<>();
-    public WorldCreator newworld = new WorldCreator("arena");
-    public Location newspawn;
     public Player[] plist;
-    PlayerInventory newinventory;
+    public boolean starting = false;
     public boolean started = false;
 
     // Spawn listener for player deaths (intercept)
@@ -41,13 +39,14 @@ public class MeekArena extends JavaPlugin {
     }
 
     public void onDisable() {
-        // cycle through arena players and restore them
-        for (Player ap : Bukkit.getWorld("arena").getPlayers()) {
-            // Restore to original state
-            this.restoreState(ap);
+        World arena = Bukkit.getWorld("arena");
 
-            // give full life and food
-            this.fullHeal(ap);
+        if (arena != null) {
+            // cycle through arena players and restore them
+            for (Player ap : arena.getPlayers()) {
+                this.restoreState(ap);
+                this.fullHeal(ap);
+            }
         }
     }
 
@@ -55,36 +54,55 @@ public class MeekArena extends JavaPlugin {
         Player splayer = Bukkit.getPlayer(sender.getName());
 
         if (cmd.getName().equalsIgnoreCase("arena")) {
-            if (args.length > 1) {
-                sender.sendMessage("Too many arguments!");
+            if (args.length > 1 || args.length < 1) {
                 return false;
-            }
-            if (args.length < 1) {
-                sender.sendMessage("Not enough arguments!");
-                return false;
-            }
-            if ("start".equals(args[0]) && this.started != true) {
+            } else if ("start".equals(args[0]) && this.started != true) {
+                if (this.starting == true) {
+                    sender.sendMessage("The arena is already starting... wait for the join message.");
+                    return true;
+                }
+
+                if (this.started == true) {
+                    sender.sendMessage("The arena is already started.  Use /arena join to enter.");
+                    return true;
+                }
+
                 Bukkit.broadcastMessage("Starting arena....");
-                // Generate new world and capture new spawn location
-                this.newworld.environment(World.Environment.NORMAL);
-                this.newworld.generateStructures(false);
-                this.newworld.type(WorldType.NORMAL);
-                this.newworld.createWorld();
-                this.newspawn = Bukkit.getWorld("arena").getSpawnLocation();
-                this.started = true;
-                Bukkit.broadcastMessage("The area is live!!! To enter, type /arena join");
+                World arena;
+                arena = Bukkit.getWorld("arena");
+
+                if (arena == null) {
+                    this.starting = true;
+                    WorldCreator wcreator = new WorldCreator("arena");
+                    wcreator.environment(World.Environment.NORMAL);
+                    wcreator.generateStructures(false);
+                    wcreator.type(WorldType.NORMAL);
+                    arena = wcreator.createWorld();
+                }
+
+                if (arena != null) {
+                    this.starting = false;
+                    this.started = true;
+                    Bukkit.broadcastMessage("The arena is live!!! To enter, type /arena join");
+                } else {
+                    getLogger().info("Failed to create arena.");
+                }
                 return true;
-            } else if ("start".equals(args[0]) && this.started == true) {
-                sender.sendMessage("The arena is already running. Use: /arena join to get into the action!");
-                return true;
-            } else if ("stop".equalsIgnoreCase(args[0]) && this.started == true) {
+            } else if ("stop".equalsIgnoreCase(args[0])) {
+                if (this.started != true) {
+                    sender.sendMessage("The arena isn't started... nothing to stop.");
+                    return true;
+                }
+                if (this.starting == true) {
+                    sender.sendMessage("The arena is starting... stop the arena once it has fully started.");
+                    return true;
+                }
+
                 List<Player> aplayers = Bukkit.getWorld("arena").getPlayers();
 
                 for (Player ap : aplayers) {
-                    // Restore to original state
+                    ap.sendMessage("You are now leaving the arena.");
                     this.restoreState(ap);
-
-                    // give full life and food
                     this.fullHeal(ap);
                 }
 
@@ -92,32 +110,68 @@ public class MeekArena extends JavaPlugin {
                 boolean result = Bukkit.unloadWorld("arena", false);
                 Bukkit.broadcastMessage("Arena ended!");
                 this.started = false;
+                this.starting = false;
                 return true;
-            } else if ("status".equalsIgnoreCase(args[0])) {
-                if (this.started == true) {
-                    sender.sendMessage("The arena is live!");
+            } else if ("join".equals(args[0])) {
+                if (this.starting == true) {
+                    sender.sendMessage("Please wait for the arena to fully start.");
+                    return true;
+                }
+                if (this.started != true) {
+                    sender.sendMessage("The arena was not started. Use /arena start to get it setup.");
+                    return true;
+                }
+                if (this.testPlayer(sender)) {
+                    // If player is already in the arena, tell them so
+                    if ("arena".equals(splayer.getLocation().getWorld().getName())) {
+                        sender.sendMessage("You are already in the arena.");
+                        return true;
+                    }
+
+                    this.saveSate(splayer);
+                    splayer.teleport(Bukkit.getWorld("arena").getSpawnLocation());
+                    this.fullHeal(splayer);
+                    this.setInventory(splayer);
                 } else {
-                    sender.sendMessage("The arena is not available.");
+                    getLogger().info("You must be a player to join the arena.");
                 }
                 return true;
-            } else if ("join".equalsIgnoreCase(args[0]) && this.started == true) {
-                this.saveSate(splayer);
-                this.setInventory(splayer);
-                splayer.teleport(newspawn);
-                this.fullHeal(splayer);
+            } else if ("leave".equals(args[0])) {
+                if (this.testPlayer(sender)) {
+                    // If player is already in the arena, tell them so
+                    if (!"arena".equals(splayer.getLocation().getWorld().getName()) || this.started != true) {
+                        sender.sendMessage("You are not in the arena.");
+                        return true;
+                    }
+
+                    splayer.teleport(Bukkit.getWorld("world").getSpawnLocation());
+                    this.fullHeal(splayer);
+                    this.restoreState(splayer);
+                } else {
+                    getLogger().info("You must be a player to leave the arena.");
+                }
                 return true;
-            } else if ("join".equalsIgnoreCase(args[0]) && this.started == false) {
-                sender.sendMessage("The arena is not available");
+            } else if ("status".equals(args[0])) {
+                String status = "";
+                String players = "";
+                if (this.started == true) {
+                    status = "started";
+                    List<Player> aplayers = Bukkit.getWorld("arena").getPlayers();
+
+                    for (Player ap : aplayers) {
+                        players += ap.getPlayerListName() + " ";
+                    }
+                } else if (this.starting == true) {
+                    status = "starting";
+                } else {
+                    status = "stopped";
+                }
+                sender.sendMessage("The arena status is: " + status);
+                sender.sendMessage("Players currently in the arena: " + players);
                 return true;
-            } else if ("leave".equalsIgnoreCase(args[0]) && this.started == true) {
-                this.restoreState(splayer);
-                this.fullHeal(splayer);
-                return true;
-            } else {
-                sender.sendMessage("/arena [start | stop | status | join | leave]");
-                return false;
             }
         }
+        //sender.sendMessage("/arena [start | stop | status | join | leave]");
         return false;
     }
 
@@ -125,31 +179,32 @@ public class MeekArena extends JavaPlugin {
         // save inventory
         this.inventories.put(inplayer.getName(), inplayer.getInventory().getContents());
         this.armors.put(inplayer.getName(), inplayer.getEquipment().getArmorContents());
-        inplayer.sendMessage("Inventory has been saved.");
+//        inplayer.sendMessage("Inventory has been saved.");
 
         // Save location
         this.locations.put(inplayer.getName(), inplayer.getLocation());
-        inplayer.sendMessage("Location has been saved.");
+//        inplayer.sendMessage("Location has been saved.");
     }
 
     public void restoreState(Player inplayer) {
         // Clear all player inventories
         inplayer.getInventory().clear();
-        inplayer.sendMessage("Inventory cleared");
+//        inplayer.sendMessage("Inventory cleared");
 
         // move all players back to "world"
+        Location spawn = new Location(this.locations.get(inplayer.getName()).getWorld(), this.locations.get(inplayer.getName()).getX(), this.locations.get(inplayer.getName()).getWorld().getHighestBlockYAt(this.locations.get(inplayer.getName())) + 2, this.locations.get(inplayer.getName()).getZ());
         inplayer.teleport(this.locations.get(inplayer.getName()));
-        inplayer.sendMessage("Restored to previous location");
+//        inplayer.sendMessage("Restored to previous location");
 
         // reinstate all original inventory
         inplayer.getInventory().setContents(this.inventories.get(inplayer.getName()));
         inplayer.getEquipment().setArmorContents(this.armors.get(inplayer.getName()));
-        inplayer.sendMessage("Inventory restored");
+//        inplayer.sendMessage("Inventory restored");
     }
 
     public void setInventory(Player inplayer) {
         inplayer.getInventory().clear();
-        inplayer.sendMessage("Inventory has been cleared");
+//        inplayer.sendMessage("Inventory has been cleared");
 
         // Setup default inventory
         PlayerInventory inventory = inplayer.getInventory();
@@ -164,13 +219,22 @@ public class MeekArena extends JavaPlugin {
         inplayer.getEquipment().setChestplate(new ItemStack(Material.CHAINMAIL_CHESTPLATE, 1));
         inplayer.getEquipment().setHelmet(new ItemStack(Material.CHAINMAIL_HELMET, 1));
         inplayer.getEquipment().setLeggings(new ItemStack(Material.CHAINMAIL_LEGGINGS, 1));
-        inplayer.sendMessage("Inventory has been built");
+//        inplayer.sendMessage("Inventory has been built");
     }
 
     public void fullHeal(Player inplayer) {
         // give full life and food
+        inplayer.setFireTicks(0);
         inplayer.setHealth(inplayer.getMaxHealth());
         inplayer.setFoodLevel(20);
         inplayer.setExhaustion(0);
+    }
+
+    public boolean testPlayer(CommandSender sender) {
+        if (sender instanceof Player) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- src/org/meekers/plugins/meekarena/MeekArenaPluginListener.java
+++ src/org/meekers/plugins/meekarena/MeekArenaPluginListener.java
@@ -1,12 +1,15 @@
 package org.meekers.plugins.meekarena;
 
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Entity;
+import org.bukkit.Location;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 /**
@@ -23,48 +26,59 @@ class MeekArenaPluginListener implements Listener {
     }
 
     @EventHandler
-    public void onDeath(EntityDamageByEntityEvent event) {
-        String aname = null;
+    public void onDeath(EntityDamageEvent event) {
         String rname = null;
 
-        if (event.getEntity().getType() == EntityType.PLAYER) {
+        if (event.getEntity() instanceof Player) { // .getType() == EntityType.PLAYER) {
             Player receiver = (Player) event.getEntity();
             rname = receiver.getPlayerListName();
+            String pworld = receiver.getWorld().getName();
 
-            if (event.getDamager().getType() == EntityType.PLAYER) {
-                Player attacker = (Player) event.getDamager();
-                aname = attacker.getPlayerListName();
-            } else {
-                Entity attacker = event.getDamager();
-                aname = attacker.getType().getName();
-            }
 
-            int damage = event.getDamage();
+            if ("arena".equals(pworld)) {
+                int damage = event.getDamage();
+                int healthleft = receiver.getHealth() - damage;
+                //receiver.sendMessage("Dmg recd: " + damage + ", hp left: " + healthleft);                
 
-            // Detect death blow
-            if (damage > receiver.getHealth()) {
-                // Negate damage
-                event.setDamage(0);
-                
-                // Send player dead message
-                Bukkit.broadcastMessage(receiver.getPlayerListName() + " has been killed by " + aname);
+                // Detect death blow
+                if (damage > healthleft) {
+                    event.setCancelled(true);
 
-                // teleport player to spawn
-                receiver.teleport(this.plugin.newspawn);
+                    // Send player dead message
+                    Bukkit.broadcastMessage(receiver.getPlayerListName() + " died by " + event.getCause().name());
 
-                // give default inventory
-                this.plugin.setInventory(receiver);
-                
-                // full heal
-                this.plugin.fullHeal(receiver);
+                    // teleport player to spawn
+                    Location spawn = new Location(receiver.getWorld(), receiver.getWorld().getSpawnLocation().getX(), receiver.getWorld().getHighestBlockYAt(receiver.getWorld().getSpawnLocation()) + 2, receiver.getWorld().getSpawnLocation().getZ());
+                    receiver.teleport(spawn);
+
+                    // give default inventory
+                    this.plugin.setInventory(receiver);
+
+                    // full heal
+                    this.plugin.fullHeal(receiver);
+                }
             }
         }
     }
-    
+
     @EventHandler
     public void onQuit(PlayerQuitEvent event) {
         Player player = event.getPlayer();
-        this.plugin.restoreState(player);
-        this.plugin.fullHeal(player);
+        String pworld = player.getWorld().getName();
+        if ("arena".equals(pworld)) {
+            this.plugin.restoreState(player);
+            this.plugin.fullHeal(player);
+        }
+    }
+
+    @EventHandler
+    public void onWorldLeave(PlayerChangedWorldEvent event) {
+        Player player = event.getPlayer();
+        String pworld = event.getFrom().getName();
+//        player.sendMessage("you just changed from: "+pworld);
+        if ("arena".equals(pworld)) {
+            this.plugin.restoreState(player);
+            this.plugin.fullHeal(player);
+        }
     }
 }
